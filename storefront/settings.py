ff4aa48a01e926cf899ac92c5cb1225b9eef3065
Kazungu1 <kazunguderriq@gmail.com>
2023-07-18 16:26:18 +0300

    create view and map url to the view

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground'
-]
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
